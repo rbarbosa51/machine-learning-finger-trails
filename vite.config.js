30bfd293a622f8f332eb0edbeb46d39f14a0c462
Rafael Barbosa <rafael.barbosa51@gmail.com>
2023-09-01 16:31:58 -0500

    Boilerplate code

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import htmlMinifier from 'vite-plugin-html-minifier';
 
 export default defineConfig({
-    base: 'ml-finger-trails',
+    base: '/ml-finger-trails',
     server: {
         port: 8001
     },
